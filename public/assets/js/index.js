41795dca089af4764493580b8d883dc1a0f5ac46
Tolu Alimi <tolualimi05@gmail.com>
2020-06-26 00:13:57 -0500

    Styling updates

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -116,7 +116,7 @@ const renderNoteList = (notes) => {
 
     if (withDeleteButton) {
       const $delBtn = $(
-        "<i class='fas fa-trash-alt float-right text-danger delete-note'>"
+        "<i id='trash-icon' class='fas fa-trash-alt float-right text-danger delete-note'>"
       );
       $li.append($delBtn);
     }
